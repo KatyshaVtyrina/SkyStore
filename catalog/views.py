2e069ab264b996e8722129051a2d29a5bb7a9842
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-10-05 13:38:19 +0300

    correction product detail

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from catalog.models import Product
 
@@ -15,6 +15,10 @@ def display_contacts(request):
     return render(request, 'catalog/contacts.html')
 
 
-def display_product(request):
-    return render(request, 'catalog/includes/inc_product.html')
+def display_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {
+        'object': product
+    }
+    return render(request, 'catalog/includes/inc_product.html', context)
 
