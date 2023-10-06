2f235b56e39f27bde1c0c7b72bbf55968e8d8370
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-10-06 10:04:25 +0300

    add Post

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from catalog.apps import CatalogConfig
 
 from catalog.views import (display_contacts, ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView,
-                           ProductDeleteView)
+                           ProductDeleteView, PostListView)
 
 app_name = CatalogConfig.name
 
@@ -14,5 +14,6 @@ urlpatterns = [
     path('product/create/', ProductCreateView.as_view(), name='create'),
     path('product/update/<int:pk>', ProductUpdateView.as_view(), name='update'),
     path('product/delete/<int:pk>', ProductDeleteView.as_view(), name='delete'),
-    path('contacts/', display_contacts, name='contacts')
+    path('contacts/', display_contacts, name='contacts'),
+    path('blog/', PostListView.as_view(), name='post_list'),
 ]
