9de9938d52b1f65593ad02bba5237856326b863f
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-10-06 11:32:50 +0300

    add PostDetail, PostCreate, PostUpdate, PostDelete, correction success_url in Product

--- catalog/views.py
+++ catalog/views.py
@@ -16,18 +16,18 @@ class ProductDetailView(DetailView):
 class ProductCreateView(CreateView):
     model = Product
     fields = ('title', 'description', 'image', 'category', 'price')
-    success_url = reverse_lazy('catalog:home')
+    success_url = reverse_lazy('catalog:product_list')
 
 
 class ProductUpdateView(UpdateView):
     model = Product
     fields = ('title', 'description', 'image', 'category', 'price')
-    success_url = reverse_lazy('catalog:home')
+    success_url = reverse_lazy('catalog:product_list')
 
 
 class ProductDeleteView(DeleteView):
     model = Product
-    success_url = reverse_lazy('catalog:home')
+    success_url = reverse_lazy('catalog:product_list')
 
 
 def display_contacts(request):
@@ -36,3 +36,29 @@ def display_contacts(request):
 
 class PostListView(ListView):
     model = Post
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+    def get_object(self, queryset=None):
+        views = super().get_object(queryset)
+        views.increase_views_count()
+        return views
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ('name', 'slug', 'content', 'image', 'published')
+    success_url = reverse_lazy('catalog:post_list')
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ('name', 'slug', 'content', 'image', 'published')
+    success_url = reverse_lazy('catalog:post_list')
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = reverse_lazy('catalog:post_list')
