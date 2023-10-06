5c0398b4433650c42fad341282535c2abbd0b0a9
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-10-06 09:44:38 +0300

    add Post

--- catalog/models.py
+++ catalog/models.py
@@ -33,3 +33,28 @@ class Category(models.Model):
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
         ordering = ('title',)
+
+
+class Post(models.Model):
+    name = models.CharField(max_length=150, verbose_name='заголовок')
+    slug = models.CharField(max_length=150, verbose_name='slug')
+    content = models.TextField(verbose_name='содержимое')
+    image = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
+    created_at = models.DateTimeField(verbose_name='дата создания', auto_now_add=True)
+    published = models.BooleanField(verbose_name='признак публикации', default=True)
+    view_count = models.PositiveIntegerField(verbose_name='количество просмотров', default=0)
+
+    def __str__(self):
+        return f'{self.name} {self.slug}'
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+        ordering = ('-created_at',)
+
+    def increase_view_count(self):
+        """
+        Увеличивает просмотры поста на 1.
+        """
+        self.view_count += 1
+        self.save()
