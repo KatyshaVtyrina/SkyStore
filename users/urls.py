eb6b3fe378a3b6484c612e71ff9540c34c2a26e7
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-10-27 17:11:35 +0300

    add verification

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import RegisterView, ProfileView
+from users.views import RegisterView, ProfileView, verification_user
 
 app_name = UsersConfig.name
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('verification/', verification_user, name='verification'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView
 
+from users.services import send_code_email
 from users.forms import UserRegisterForm, UserProfileForm
 from users.models import User
 
@@ -11,6 +12,13 @@ class RegisterView(CreateView):
     template_name = 'users/register.html'
     success_url = reverse_lazy('users:login')
 
+    def form_valid(self, form):
+        # self.object = form.save()
+        # send_code_email(id=self.object.id, recipient_list=[self.object.email])
+        obj = form.save()
+        send_code_email(obj)
+        return super().form_valid(form)
+
 
 class ProfileView(UpdateView):
     model = User
@@ -19,3 +27,9 @@ class ProfileView(UpdateView):
 
     def get_object(self, queryset=None):
         return self.request.user
+
+
+def verification_user(request):
+    pk = request['pk']
+    user = User.objects.get(pk=pk)
+    user.is_verification = True
