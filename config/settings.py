8837ffb624b66d63dfa4b10b94f9f49176f13d6d
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-10-30 06:39:20 +0300

    update EMAIL_HOST_PASSWORD

--- config/settings.py
+++ config/settings.py
@@ -142,6 +142,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
 EMAIL_HOST_USER = 'katyshavtyrina@yandex.ru'
-EMAIL_HOST_PASSWORD = 'bugtenhqgvpaaygr'
+EMAIL_HOST_PASSWORD = 'ygfxkjbfmuilpobo'
 EMAIL_USE_SSL = True
 EMAIL_USE_TLS = False
