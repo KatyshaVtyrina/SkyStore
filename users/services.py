cb7035b6eb4aaedd1577bf9a5fe2b9d7cffbe039
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-10-30 07:28:09 +0300

    correction url

--- users/services.py
+++ users/services.py
@@ -1,13 +1,15 @@
 from django.core.mail import send_mail
-
+from django.urls import reverse_lazy
 from config import settings
 from users.models import User
 
 
 def send_code_email(user: User):
+    """Отправляет ссылку для подтверждения почты"""
     send_mail(
         'Подтверждение почты',
-        f'Чтобы подтвердить почту, перейдите по ссылке http://127.0.0.1:8000/users/verification/{user.id}',
+        f'Чтобы подтвердить почту, перейдите по этой ссылке '\
+        f'http://localhost:8000{reverse_lazy("users:verification", kwargs={"user_pk": user.pk})}',
         settings.EMAIL_HOST_USER,
         [user.email]
     )
